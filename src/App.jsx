cc6a8f0189a8e4f4e00259a0aac5feace4a465cd
ananya makwana <amakwana@wisc.edu>
2025-10-11 14:14:04 -0700

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-10 text-2xl min-h-screen flex items-center justify-center">
+      Tailwind works!
+    </div>
+  );
+}
+
+export default App;
